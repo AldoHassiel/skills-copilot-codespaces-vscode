390941197b7866aab3c1167cde44f4d0b405702c
Aldo Hassiel <110874704+AldoHassiel@users.noreply.github.com>
2023-11-05 01:30:08 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,18 @@
+function skillmember() {
+    var member = document.getElementById("member");
+    var skill = document.getElementById("skill");
+    var project = document.getElementById("project");
+    var contact = document.getElementById("contact");
+    var member1 = document.getElementById("member1");
+    var skill1 = document.getElementById("skill1");
+    var project1 = document.getElementById("project1");
+    var contact1 = document.getElementById("contact1");
+    member.style.display = "block";
+    skill.style.display = "none";
+    project.style.display = "none";
+    contact.style.display = "none";
+    member1.style.color = "#fff";
+    skill1.style.color = "#000";
+    project1.style.color = "#000";
+    contact1.style.color = "#000";
+}  
