63e9af90d0cc4c337a079e527e162903c1d15670
Aldo Hassiel <110874704+AldoHassiel@users.noreply.github.com>
2023-11-05 01:36:46 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,18 +1,12 @@
 function skillmember(){
-    var member = document.getElementById("member");
-    var skill = document.getElementById("skill");
-    var project = document.getElementById("project");
-    var contact = document.getElementById("contact");
-    var member1 = document.getElementById("member1");
-    var skill1 = document.getElementById("skill1");
-    var project1 = document.getElementById("project1");
-    var contact1 = document.getElementById("contact1");
-    member.style.display = "block";
-    skill.style.display = "none";
-    project.style.display = "none";
-    contact.style.display = "none";
-    member1.style.color = "white";
-    skill1.style.color = "black";
-    project1.style.color = "black";
-    contact1.style.color = "black";
+    return{
+        restrict: 'E',
+        templateUrl: 'app/components/skillmember/skillmember.html',
+        controller: 'SkillMemberController',
+        controllerAs: 'vm',
+        bindToController: true,
+        scope: {
+            member: '='
+        }
+    };
 }
