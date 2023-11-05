9aafd5653dd2b525e6829e17dbc948136c026bfc
Aldo Hassiel <110874704+AldoHassiel@users.noreply.github.com>
2023-11-05 01:37:53 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,12 +1,3 @@
 function skillmember(){
-    return{
-        restrict: 'E',
-        templateUrl: 'app/components/skillmember/skillmember.html',
-        controller: 'SkillMemberController',
-        controllerAs: 'vm',
-        bindToController: true,
-        scope: {
-            member: '='
-        }
-    };
+    return "skillmember";
 }
